c7e4c8a1ba488a4e103e92f80d0e65974fc67e5d
Vidya Khadsare <vidya.khadsare@gmail.com>
2016-06-10 10:56:49 -0700

    first commit

---
+++ JS/angscript.js
@@ -0,0 +1,13 @@
+var myApp = angular.module("myModule", [])
+				   .controller("myController", function($scope){
+	
+	var background = {
+			backgroundImage: "img/bk4.jpeg" 
+
+	}
+
+	$scope.background = background;
+
+	
+});
+
